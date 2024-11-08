2b520a170b930a25403ea47d6f4b03011d3bd044
JayDawPo <edryljaycabanatan14@gmail.com>
2024-11-08 22:57:02 +0400

    Delete #Exercise 6.py

--- #Exercise 6.py
+++
@@ -1,8 +0,0 @@
-#Exercise 6
-correct_password = "SirOliverPogi"
-user_password = input("Enter the password: ")
-if user_password == correct_password:
-    print("Password is correct! Access granted.")
-else:
-    print("Incorrect password. Please try again.")
-        
